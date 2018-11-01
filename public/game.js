36e9afd82405640b8c8f37ac34b5221479428c67
xianchen <xianchen>
2018-11-01 09:32:27 -0700

    Prevent two players from joining and removal when disconnection

--- public/game.js
+++ public/game.js
@@ -1,48 +1,107 @@
-var socket = io();
+//Vars
+let socket = io();
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
 
-var movement = {
-  key_code: 0
-}
+//Hooks
+document.onmousemove = function(e) {
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+};
 
-document.addEventListener('keypress', function(event) {
-    switch (event.keyCode) {
-      case 97: // A
-         movement.key_code = 97;
-        break;
-      case 119: // W
-          movement.key_code = 119;
-        break;
-      case 100: // D
-        movement.key_code = 100;
+//Listen for keypress, Tanks only go left and right.
+document.addEventListener("keypress", function(event) {
+  switch (event.keyCode) {
+    case 97: // A
+      movement.key_code = 97;
       break;
-      case 115: // S
-        movement.key_code = 115
-        break;
-    } 
-  });
+    case 100: // D
+      movement.key_code = 100;
+      break;
+  }
+});
 
-  socket.emit('new player');
-  setInterval(function() {
-    socket.emit('movement', movement);
-  }, 1000 / 60);
+document.addEventListener("keyup", function(event) {
+  movement.key_code = 0;
+});
+
+//Keycode pressed for each browser
+let movement = {
+  key_code: 0
+};
+
+let mouse = {
+  x: 0,
+  y: 0,
+  angle: 0
+};
+
+let tank = function(x, y) {
+  context.beginPath();
+  context.rect(x, y, 40, 20);
+  context.fill();
+};
+
+//Constantly update player movement unique to each socket.
+socket.emit("new player");
+setInterval(function() {
+  socket.emit("movement", movement);
+}, 1000 / 60);
 
 
-  
+//Game State
+socket.on("state", function(players) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = "green";
+  //Render out tank for each player
+  for (var id in players) {
+    //Select player logged in
+    var player = players[id];
+    tank(player.x, player.y);
+    //Create barrel to follow on mouse movement
+    // context.globalCompositeOperation = "lighter";
+    // context.strokeStyle = "#a00";
+    // context.lineWidth = Math.random() * 2;
+    // context.beginPath();
+    // context.moveTo(325, 5)
+    // context.lineTo(mouse.x, 30);
+    // context.stroke();
+    // context.globalCompositeOperation = "source-over";
+  }
+});
 
- 
-  var canvas = document.getElementById('canvas');
-  canvas.width = 800;
-  canvas.height = 600;
-  var context = canvas.getContext('2d');
-  var gun = canvas.getContext('2d');
-  socket.on('state', function(players) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'green';
-    for (var id in players) {
-      var player = players[id];
-      context.beginPath();
-      context.rect(player.x,player.y,40,20);
-      context.fill();
+
+
+let lookAt = {
+  calc: function(x, y, lookX, lookY) {
+    if (x == lookX && y == lookY) return 0;
+
+    x = lookX - x;
+    y = lookY - y;
+
+    var angle = this.angle(x, y, 1, 0);
+
+    if (y < 0) {
+      return 2 * Math.PI - angle;
+    } else {
+      return angle;
     }
-  });
+  },
 
+  // utilities
+  length: function(x, y) {
+    return Math.sqrt(x * x + y * y);
+  },
+
+  dot: function(ax, ay, bx, by) {
+    return ax * bx + ay * by;
+  },
+
+  angle: function(ax, ay, bx, by) {
+    return Math.acos(
+      this.dot(ax, ay, bx, by) / (this.length(ax, ay) * this.length(bx, by))
+    );
+  }
+};
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var http = require("http");
-var path = require("path");
-var socketIO = require("socket.io");
-var app = express();
-var server = http.Server(app);
+let express = require("express");
+let http = require("http");
+let path = require("path");
+let socketIO = require("socket.io");
+
+let app = express();
+let server = http.Server(app);
+let firstPlayer = true;
+let maxPlayers = 1;
 var io = socketIO(server);
-let max = true;
 app.set("port", 5000);
 app.use("/static", express.static(__dirname + "/public"));
 // Routing
@@ -17,57 +19,65 @@ server.listen(5000, function() {
   console.log("Starting server on port 5000");
 });
 
-var players = {};
-var allClients = [];
+let players = {};
+let allClients = [];
 io.on("connection", function(socket) {
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function() {
     console.log(socket.id);
     var i = allClients.indexOf(socket);
     allClients.splice(i, 1);
- });
+    console.log(players[socket.id])
+    delete players[socket.id];
+  });
 
   socket.on("new player", function() {
-    if(max){
+    if (firstPlayer && (maxPlayers <= 2)) {
       players[socket.id] = {
         x: 300,
-        y: 0,
+        y: 0
       };
-      max = false;
-  }
-  else{
-    players[socket.id] = {
-      x: 300,
-      y: 575,
-    };
-    max = true;
-  }
+      firstPlayer = false;
+      maxPlayers++;
+      console.log('first player');
+    } else if (!firstPlayer && (maxPlayers <= 2)) {
+      players[socket.id] = {
+        x: 300,
+        y: 575
+      };
+      console.log('second player')
+      firstPlayer = true;
+      maxPlayers++;
+    }
+    else{
+      console.log
+    }
   });
+
   socket.on("movement", function(data) {
     var player = players[socket.id] || {};
     if (data.key_code == 97) {
-      if(player.x >= 5){
+      if (player.x >= 5) {
         player.x -= 5;
       }
     }
     if (data.key_code == 119) {
-      if(player.y >= 5){
-      player.y -= 5;
+      if (player.y >= 5) {
+        player.y -= 5;
       }
-      
     }
     if (data.key_code == 100) {
-      if(player.x <= 600){
-      player.x += 5;
+      if (player.x <= 600) {
+        player.x += 5;
       }
-      }
+    }
     if (data.key_code == 115) {
-      if(player.y <= 575 ){
-      player.y += 5;
+      if (player.y <= 575) {
+        player.y += 5;
       }
     }
   });
 });
 
 setInterval(function() {
-  io.sockets.emit('state', players);
+  io.sockets.emit("state", players);
 }, 1000 / 60);
